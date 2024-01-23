9d48d2660d2566f539bf7fcee735ba556f007ac7
Evgeny Eysner <evgeny.eysner@gmail.com>
2024-01-23 08:12:49 +0100

    feat: create frontend logics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Result } from './pages/Result';
+import { Scanner } from './pages/Scanner';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Routes>
+			<Route path='/' element={<Scanner />} />
+			<Route path='/result/:name' element={<Result />} />
+
+			<Route path='*' element={<Navigate to='/' />} />
+		</Routes>
+	);
 }
 
 export default App;
