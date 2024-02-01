4098b95f1eef1b3882856bdecbb1a456d1f82823
Evgeny Eysner <evgeny.eysner@gmail.com>
2024-02-01 11:09:58 +0100

    fix: service worker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,28 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import './App.css';
-import { Main } from './pages/Main';
-import { Result } from './pages/Result';
-import { Scanner } from './pages/Scanner';
+import { Navigate, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Main } from './pages/Main'
+import { Result } from './pages/Result'
+import { Scanner } from './pages/Scanner'
 
-function App() {
-	return (
-		<Routes>
-			<Route path='/' element={<Main />} />
-			<Route path='/scan' element={<Scanner />} />
-			<Route path='/result/:name' element={<Result />} />
-
-			<Route path='*' element={<Navigate to='/' />} />
-		</Routes>
-	);
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('./serviceworker.js')
+      .then((reg) => console.log('Successfully registered: ', reg.scope))
+      .catch((err) => console.log('Error registering: ', err))
+  })
 }
 
-export default App;
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/scan" element={<Scanner />} />
+      <Route path="/result/:name" element={<Result />} />
+
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  )
+}
+
+export default App
