64b8219193500409fd145bbdc3e665c15bda5a16
Evgeny Eysner <evgeny.eysner@gmail.com>
2024-02-11 18:57:34 +0100

    feat: add ALLOWED_HOSTS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,8 @@ BASE_URL = env("DJANGO_SERVER_URL")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
+# ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["demo.softeis.net"]
 
 # Application definition
 DJANGO_APPS = (
