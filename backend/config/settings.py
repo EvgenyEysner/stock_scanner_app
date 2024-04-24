243913f88dda95e70e533389e52e7b0b586d626f
Evgeny Eysner <eeysner@dataahead.de>
2024-04-24 18:21:19 +0200

    fix: cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -159,32 +159,33 @@ STATICFILES_DIRS = ("static",)
 FRONTEND_URL = env("DJANGO_FRONTEND_URL")
 CORS_ALLOW_ALL_ORIGINS = True
 # CORS
-# CORS_ORIGIN_WHITELIST = env.list("DJANGO_CORS_ORIGIN_WHITELIST")
+CORS_ORIGIN_WHITELIST = "https://bucolic-pixie-739bce.netlify.app"
 CORS_ALLOWED_ORIGINS = (
     "https://bucolic-pixie-739bce.netlify.app",
     "http://localhost:3000",
 )
-# CORS_ALLOW_CREDENTIALS = True
-# CORS_ALLOW_METHODS = default_methods
-#
-# CORS_ALLOW_HEADERS = [
-#     "accept",
-#     "accept-encoding",
-#     "authorization",
-#     "content-type",
-#     "dnt",
-#     "origin",
-#     "user-agent",
-#     "x-csrftoken",
-#     "x-requested-with",
-# ]
-#
-# CORS_EXPOSE_HEADERS = [
-#     "accept",
-#     "accept-encoding",
-#     "authorization",
-#     "content-type",
-# ]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = default_methods
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CORS_EXPOSE_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
