385294a9a3879e8896f709b16baf9dcfde1f88fb
Evgeny Eysner <eeysner@dataahead.de>
2024-08-28 08:27:48 +0200

    fix: notify task

--- backend/apps/api/tasks.py
+++ backend/apps/api/tasks.py
@@ -1,36 +1,9 @@
-import csv
-import io
-
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import EmailMessage
 
+from apps.api.utils.csv_service import generate_csv
 from apps.warehouse.models import Order
-
-
-def generate_csv(order_id: int):
-    order = Order.objects.get(id=order_id)
-    file = io.StringIO()
-    writer = csv.writer(file, delimiter=",", quoting=csv.QUOTE_ALL)
-    writer.writerow(
-        (
-            "Bezeichnung",
-            "Menge",
-            "Lager",
-            "EAN",
-        )
-    )
-    for order_item in order.items.all():
-        writer.writerow(
-            (
-                order_item.item.name,
-                order_item.quantity,
-                order_item.item.stock,
-                order_item.item.ean,
-            )
-        )
-    file.seek(0)
-    return file
 
 
 @shared_task
@@ -54,3 +27,8 @@ def order_created(order_id: int):
     )
     email.attach(f"auftrag_{order.id}.csv", file.getvalue(), "text/csv")
     email.send(fail_silently=False)
+
+
+@shared_task
+def check_stock():
+    pass
---
+++ backend/apps/api/utils/csv_service.py
@@ -0,0 +1,29 @@
+import csv
+import io
+
+from apps.warehouse.models import Order
+
+
+def generate_csv(order_id: int):
+    order = Order.objects.get(id=order_id)
+    file = io.StringIO()
+    writer = csv.writer(file, delimiter=",", quoting=csv_service.QUOTE_ALL)
+    writer.writerow(
+        (
+            "Bezeichnung",
+            "Menge",
+            "Lager",
+            "EAN",
+        )
+    )
+    for order_item in order.items.all():
+        writer.writerow(
+            (
+                order_item.item.name,
+                order_item.quantity,
+                order_item.item.stock,
+                order_item.item.ean,
+            )
+        )
+    file.seek(0)
+    return file
